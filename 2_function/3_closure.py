692dd9f4fae73f3d3d4f278a705f753a28fd45d2
Sunsik <siksicking@gmail.com>
2020-08-19 10:47:05 +0900

    On closure

---
+++ 2_function/3_closure.py
@@ -0,0 +1,44 @@
+"""
+< Closure : A function that remembers namespace of enclosing scope >
+    -> That is, for a function to be a closure, it must satisfy following:
+
+    1. It is nested function of another function
+    2. It uses variable defined in namespace of enclosing scope(i.e. free variable in nonlocal scope)
+    3. It is returned as an output of enclosing function
+"""
+
+import random
+
+
+def WLLN(round_at):
+    """
+    Function to stack random numbers and return their sample mean to ensure it converges to population mean.
+    Note that 'average_in_action' function is
+        1. a nested function of 'WLLN'
+        2. using free variable defined in namespace of 'WLLN'
+        3. returned as an output of 'WLLN'
+    """
+    numbers = []
+    def average_in_action(value):
+        numbers.append(value)
+        return round(sum(numbers) / len(numbers), round_at)
+    return average_in_action
+
+
+"""
+In the code below, 'average_in_action' is executed 320 times, where each time random number from Unif(0,1) is generated and stored in 'numbers' defined in nonlocal scope.
+Since it returns sample mean of it, user can observe that it indeed converges in probability to 0.5
+
+Note that there are two free variables in the nonlocal scope: numbers, round_at.
+Names and their values can be observed by using specific methods written below.
+"""
+random.seed(1234)
+averager = WLLN(round_at=4)
+for iter in range(320):
+    result = averager(random.random())
+    if (iter+1) % 40 == 0:
+        print(f"| iteration : {str(iter+1).rjust(3)} | sample mean : {str(result).ljust(6,'0')} |")
+
+print(averager.__code__.co_freevars)
+print(len(averager.__closure__[0].cell_contents))
+print(averager.__closure__[1].cell_contents)
